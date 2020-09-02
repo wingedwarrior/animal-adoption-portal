12470b2e9cdd45afa4a932264879b97ea0ed3b93
Chris Seymour <Chris.Seymour@ccc.govt.nz>
2020-09-03 11:47:24 +1200

    Changed rock

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,12 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            //new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
+            //new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
+            //new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Bob", Age = 7, Description = "The gaurdian" },
+            new Animal { Id = 2, Name = "Mary", Age = 6, Description = "Mum" },
+            new Animal { Id = 3, Name = "Ted", Age = 2, Description = "Child 1" },
         };
     }
 }
--- AnimalAdoption.Common.Logic/LoginService.cs
+++ AnimalAdoption.Common.Logic/LoginService.cs
@@ -15,7 +15,7 @@ namespace AnimalAdoption.Common.Logic
                 "icecream",
                 "basketball",
                 "orange",
-                "placeholder"
+                "cat"
             };
         }
 
@@ -40,8 +40,8 @@ namespace AnimalAdoption.Common.Logic
                 case "orange":
                     name = "Murphy";
                     break;
-                case "placeholder":
-                    name = "Placeholder";
+                case "cat":
+                    name = "Fred";
                     break;
             }
             return name;
